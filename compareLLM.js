4e0641a2d62f3ffd1cbe2470c0bde32d2da24f57
ivanVgusev <>
2025-12-08 00:43:08 +0300

    ADD & UPD: files for comparison & multicomp tool

--- compareLLM.js
+++ compareLLM.js
@@ -1,6 +1,4 @@
-var natural = require('natural');
-var tokenizer = new natural.WordTokenizer();
-
+// обозначение функций, которые нужно дописать для полноценной работы
 function tagsRemoval(text) {
     // нужна функция для удаления тэгов docx/pdf
     return text
@@ -13,10 +11,10 @@ function OCR(pdf) {
 }
 
 
+// вспомогательные функции
 function isArraysEqual(firstArray, secondArray) { 
     return firstArray.toString() === secondArray.toString(); 
 }
-
 
 function textNormalization(text) {
     var normalizedText;
@@ -33,7 +31,7 @@ function textNormalization(text) {
 }
 
 // ngramSize — размер "окна", то есть, количество слов, которое будет в одной группе
-function textToNgrams(normalizedText, ngramSize = 6, hopLength = 3) {
+function textToNgrams(normalizedText, ngramSize = 10, hopLength = 1) {
     var textSplit = normalizedText.split(" ");
     var ngramGroupsAmount = textSplit.length / ngramSize;
     // в хвосте может может оставаться n-грамма с меньшим числом элементов
@@ -65,16 +63,69 @@ function compareStrings(oldText, newText) {
     return resultLines.join('\n');
 }
 
-var text1 = "Но солнечный луч танцевал на полированной столешнице, освещая крошку от только что съеденного круассана.";
-var text11 = "Солнечный луч скользил по полированной столешнице, освещая крошку от съеденного круассана."
-var text2 = "Машина летела по трассе и не хотела останавливаться. Все были в ужасе и не знали, что же делать.";
+
+function diffText(text1, text2) {
+    var wordText1 = text1[0];
+    var wordText2 = text2[0];
+    
+    var countShift = 0
+    while (wordText1 != wordText2) {
+
+        countShift++
+    }
+    return countShift
+}
+
+
+function longestSubsequence(oldTextNgram, newTextNgram) {
+    var sameNgramIdxOne = [];
+    var sameNgramIdxTwo = [];
+    for (var ngramTextOne = 0; ngramTextOne < oldTextNgram.length; ngramTextOne++) {
+        for (var ngramTextTwo = 0; ngramTextTwo < newTextNgram.length; ngramTextTwo++) {
+            if (isArraysEqual(oldTextNgram[ngramTextOne], newTextNgram[ngramTextTwo])) {
+                sameNgramIdxOne.push(ngramTextOne);
+                sameNgramIdxTwo.push(ngramTextTwo);
+            }
+        }
+    }
+    var idealText1 = [...Array(oldTextNgram.length).keys()];
+    var diffNgramIdxOne = idealText1.filter(item => !sameNgramIdxOne.includes(item));
+
+    var idealText2 = [...Array(newTextNgram.length).keys()];
+    var diffNgramIdxTwo = idealText2.filter(item => !sameNgramIdxTwo.includes(item));
+
+    // return [sameNgramIdxOne, sameNgramIdxTwo]
+    return [diffNgramIdxOne, diffNgramIdxTwo]
+}
+
+
+var fs = require('fs');
+
+try {
+    var text1 = fs.readFileSync('testTextOneCompareLLM.txt', 'utf8');
+} catch (err) {
+    console.error('Ошибка чтения файла:', err);
+}
+
+try {
+    var text2 = fs.readFileSync('testTextTwoCompareLLM.txt', 'utf8');
+} catch (err) {
+    console.error('Ошибка чтения файла:', err);
+}
+
 
 var text1Normalized = textNormalization(text1);
-var text11Normalized = textNormalization(text11);
 var text2Normalized = textNormalization(text2);
 
 var text1Ngrams = textToNgrams(text1Normalized);
-var text11Ngrams = textToNgrams(text11Normalized);
 var text2Ngrams = textToNgrams(text2Normalized);
 
-console.log(compareStrings(text11Normalized, text1Normalized))
+// console.log(compareStrings(text11Normalized, text1Normalized))
+// console.log(compareStrings(text1Normalized, text11Normalized))
+
+// console.log(text11Ngrams[0], text11Ngrams[1], text11Ngrams[2], text11Ngrams[3])
+// console.log(text1Ngrams[0], text1Ngrams[1], text1Ngrams[2])
+
+console.table(longestSubsequence(text1Ngrams, text2Ngrams))
+
+console.log(compareStrings(text1Normalized, text2Normalized))
